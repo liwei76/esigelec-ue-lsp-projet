38fba78baa83783d4d44d0fde094aca165aa658f
dinglingzhou <58291682+dinglingzhou@users.noreply.github.com>
2020-01-19 10:05:32 -0800

    Add files via upload

---
+++ spark-streaming/scripts/producer.py
@@ -0,0 +1,15 @@
+import csv
+import time
+from kafka import KafkaProducer
+
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+csvfile = open("../data/user_log.csv", "r")
+reader = csv.reader(csvfile)
+
+for line in reader:
+    gender = line[9]
+    if gender == 'gender':
+        continue
+    time.sleep(0.1)
+    producer.send('sex', line[9].encode('utf8'))
